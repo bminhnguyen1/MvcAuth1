39f7997aa9cf4593baf3dff99a2ec0ba4c7a96d7
buinguyenminhnguyenitc <guunee10@gmail.com>
2019-07-25 12:00:47 +0700

    Initial Commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAuth1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAuth1.Startup))]
+namespace MvcAuth1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
